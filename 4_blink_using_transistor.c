e538a7b2224cbbca7ce8ce1dd10de93943fb937b
Harpreet Sangar <sangarhappy@gmail.com>
2018-07-25 17:12:39 +0530

    comments corrected

--- 4_blink_using_transistor.c
+++ 4_blink_using_transistor.c
@@ -9,19 +9,19 @@ int main()
 	if(-1 == wiringPiSetupPhys())// if the fuction returns -1 some problem has occured.
 		return 1;
 
-	// sets the pin 12 for output
+	// sets the pin 37 for output
 	pinMode(BASE_PIN, OUTPUT);
 
 	// loop to run forever
 	for(;;)
 	{
-		// set pin 12 / base pin to HIGH. when base pin is set HIGH, the LED lights up.
+		// set pin 37 / base pin to HIGH. when base pin is set HIGH, the LED lights up.
 		// Here common emitter configuration is used to make transistor act like a switch.
 		digitalWrite(BASE_PIN, HIGH);
 		// delays the program by 300 ms
 		delay(300);
 
-		// set pin 12 / base pin to LOW. when base pin is set LOW, the LED stops glowing.
+		// set pin 37 / base pin to LOW. when base pin is set LOW, the LED stops glowing.
 		digitalWrite(BASE_PIN, LOW);
 		// delays the program by 100 ms
 		delay(100);
